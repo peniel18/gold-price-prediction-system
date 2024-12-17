6f6460fee0fbc0254f7920573c45f18ea9a81820
peniel18 <penieletornam18@gmail.com>
2024-12-17 11:37:32 +0000

    project files

--- template.py
+++
@@ -1,7 +0,0 @@
-import os 
-from typing import List 
-
-
-list_of_files: List[str] = [
-    
-]
---
+++ templates.py
@@ -0,0 +1,20 @@
+import os 
+
+project_name = "gold"
+
+list_of_files = [
+    f"{project_name}/components/data_ingestion.py", 
+    f"{project_name}/__init__.py"
+    f"{project_name}/components/__init__.py", 
+    f"{project_name}/components/data_transformation.py", 
+    f"{project_name}/components/model_trainer.py", 
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/utility_functions.py",
+    f"{project_name}/exception/__init__.py", 
+    f"{project_name}/exception/exception.py", 
+    f"{project_name}/logging/__init__.py"
+    "app.py", 
+    "notebooks/experiments.ipynb", 
+    ".env", 
+    "Artifacts"
+]
