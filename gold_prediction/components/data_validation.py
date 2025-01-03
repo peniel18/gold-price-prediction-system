daf4621503cd88eab0e8f40c6a33087e49707565
peniel18 <penieletornam18@gmail.com>
2025-01-03 01:44:56 +0000

    drift concepts

--- gold_prediction/components/data_validation.py
+++ gold_prediction/components/data_validation.py
@@ -2,7 +2,8 @@ from gold_prediction.logging.logger import logging
 from gold_prediction.exception.exception import CustomException 
 #from evidently.dashboard import Dashboard 
 from scipy.stats import ks_2samp
-from typing import List
+from omegaconf import OmegaConf
+from typing import List, Tuple
 import sys 
 import pandas as pd 
 import json 
@@ -17,9 +18,18 @@ class DataValidation:
     def __init__(self, dataValidationConfig):
         self.dataValidationConfig = dataValidationConfig 
 
-    def evaluate_data_drift(self, base_dataset, incoming_dataset, threshold):
+    def evaluate_data_drift(self,
+                            base_dataset: pd.DataFrame,
+                            incoming_dataset: pd.DataFrame,
+                             threshold: float = 0.05 
+                             ) -> List[dict]:
         """
         Calculates Data drift for incoming data 
+
+        Args: 
+            base_dataset: Reference dataset used as baseline 
+            incoming_dataset: New Dataset to compare against base
+            threshold: Threshold for KS Statistic to determine drift
 
         Returns: 
             the p-value of statistical test performed for each feature
@@ -34,34 +44,47 @@ class DataValidation:
                 base = base_dataset[column]
                 current = incoming_dataset[column]
                 ks_stat, p_value = ks_2samp(base, current)
-                dataDrift = ks_stat > threshold 
+                # we reject the null hypothesis, there is statistical evidence of drift
+                dataDrift = p_value <= threshold 
                 # log drifts of each colum to mlflow 
                 column_drift: dict = {}
                 column_drift["column"] = column
                 column_drift["P-value"] = p_value
                 column_drift["KS statistic"] = ks_stat
+                column_drift["drift detected"] = dataDrift
 
                 data_drift_report.append(column_drift)
 
+            # log experiments to mlflow 
 
-            return dataDrift, data_drift_report
+
+
+
+            return data_drift_report
         
         except Exception as e: 
             logging.error("Error occurred during evaulation of drifts")
             raise CustomException(e, sys)
 
 
-    def log_experiments_to_mflow(self):
-        pass 
-
     def validateFeatureColumns(self):
-        pass 
+
+        return None 
 
     def InitiateDataValidaton(self):
-        pass 
+        trainData = pd.read_csv(self.dataValidationConfig.train_data.path)
+        testData =  pd.read_csv(self.dataValidationConfig.test_data.path)
+
+        DataDriftReport = self.evaluate_data_drift(
+            base_dataset=trainData, 
+            incoming_dataset=testData, 
+        )
+        print(DataDriftReport)
 
 
         
 
 if __name__ == "__main__":
-    pass 
+    dataValConfig = OmegaConf.load("configs/data_validation.yaml")
+    dataValidation = DataValidation(dataValidationConfig=dataValConfig)
+    dataValidation.InitiateDataValidaton()
