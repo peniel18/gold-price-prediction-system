42fdd42827041f215cadfe376c1b3d49eabe792d
peniel18 <penieletornam18@gmail.com>
2025-01-08 23:32:10 +0000

    added functions

--- gold_prediction/components/model_trainer.py
+++ gold_prediction/components/model_trainer.py
@@ -5,8 +5,26 @@ from gold_prediction.exception.exception import CustomException
 
 
 class ModelTrainer: 
-    def __init__(self, ModelTrainerConfig):
+    def __init__(self, 
+                 ModelTrainerConfig, 
+                 tune_hyperparameters: bool | None = True):
         self.ModelTrainerConfg = ModelTrainerConfig
+        self.tune_hyperparameters = tune_hyperparameters
+
+    def get_training_data(self):
+        return None 
+
+    def save_model_locally(self):
+        pass 
+
+    def register_models_on_hopswork(self):
+        pass  
+    
+    def train(self):
+        pass 
+
+
+
 
 
     def InitiateModelTrainer(self):
