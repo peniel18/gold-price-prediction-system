6405f24a4567966ff313cbc36b38fbc6e1a9020a
peniel18 <penieletornam18@gmail.com>
2025-04-01 00:22:43 +0000

    inference model

--- gold_prediction/components/model_evaluation.py
+++ gold_prediction/components/model_evaluation.py
@@ -2,7 +2,9 @@ from gold_prediction.logging.logger import logging
 from gold_prediction.exception.exception import CustomException
 import os 
 import hopsworks
+from sklearn import metrics 
 import sys
+from typing import Tuple
 import pandas as pd 
 
 
@@ -25,6 +27,7 @@ class ModelEvaluation:
         """
         model_registry = self.HOPSWORKS_PROJECT.get_model_registry()
         model = model_registry.get_model(name=name, version=None)
+        model = model.download()
         return model 
     
     
@@ -64,14 +67,51 @@ class ModelEvaluation:
                 description="gold_prediction_test_data"
             )
             return data 
+        
+
+    def prepare_data_for_inference(self, data: Tuple[pd.DataFrame]) -> pd.DataFrame:
+        df = data[0]
+        columns = [
+            'close', 'agg_mean', 'agg_max', 'agg_std', 'agg_min', 'kurt',
+            'skewness', 'month', 'year', 'day', 'dayofweek', 'is_weekend', 'dayofyear', 'quarter'
+            ]
+        
+        return df[columns]
+    
+
+    def model_inference(self, data:pd.DataFrame) -> dict: 
+        data = self.get_inference_data()
+        df = self.prepare_data_for_inference(data)
+        model = self.load_model()
+
+        yTest = df["close"]
+        XTest = df.drop("close", axis="columns")
+        
+        yHat = model.predict(XTest)
+
+        mse = metrics.mean_squared_error(yTest, yHat)
+        rmse = metrics.root_mean_squared_error(yTest, yHat)
+        mape = metrics.mean_absolute_percentage_error(yTest, yHat)
+
+        metrics = {
+            "Mean Squared Error": mse, 
+            "Root Mean Squared Error": rmse, 
+            "Mean Absolute Percentage Error" : mape
+        }
+
+        return metrics
+
+
+    def log_metrics(self):
+        pass 
 
     def InitializeModelEvaluation(self):
         model = self.load_model()
         print(model)
         data = self.get_inference_data()
-
-        print(model)
         print(data)
+        metrics = self.model_inference(data)
+        print(metrics)
 
 
 if __name__ == "__main__":
--- gold_prediction/pipeline/batch_prediction.py
+++ gold_prediction/pipeline/batch_prediction.py
@@ -1,1 +1,4 @@
 from gold_prediction.exception.exception import CustomException
+
+
+### use hopsworks batch prediction feature 
