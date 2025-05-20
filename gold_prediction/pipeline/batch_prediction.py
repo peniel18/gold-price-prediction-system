75a967ff392eeefb4e45f7bec0be9f69b3b6aa39
peniel18 <penieletornam18@gmail.com>
2025-05-20 15:05:32 +0000

    batch pipeline

--- gold_prediction/pipeline/batch_prediction.py
+++ gold_prediction/pipeline/batch_prediction.py
@@ -13,7 +13,6 @@ import os
 # 4. build an api to fetch predictions upon demand
 
 
-
 class BatchPredictionsPipeline: 
     def __init__(self, BatchPredsConfig):
         self.BatchPredsConfig = BatchPredsConfig
@@ -22,18 +21,41 @@ class BatchPredictionsPipeline:
         )
 
 
-    def get_predictions_data(self):
+    def get_predictions_data(self, name, description):
         """
         Gets data from feature store for batch predictions 
+       
+        Args: 
+            name (str): 
+            description (str): 
+        
         """
-        feature_store = self.hopsworks_project.get_feature_store()
+        try: 
+            feature_store = self.hopsworks_project.get_feature_store()
+            feature_view = feature_store.get_feature_view(name=description)
+            data = feature_view.get_batch_data(
+                start_time = None , 
+                end_time = None 
+            )
+            return data 
+        except: 
+            logging.info("Creating a Feature View for batch predictions data")
+            feature_store = self.hopsworks_project.get_feature_store()
+            feature_group = feature_store.get_feature_group(name=name)
+            columns_to_query = feature_group.select_all()
+            feature_view = feature_store.create_feature_view(
+                 name = description, 
+                 query=columns_to_query
+            )
+
+            data = feature_view.get_batch_data(
+                start_time = None , 
+                end_time = None
+            )
+            return data
 
 
 
-
-
-
-    
     def load_model_from_model_registry(self, hopsworks_project, model_version: int = 1):
         """
         Loads model from Hopsworks model registry
@@ -47,7 +69,7 @@ class BatchPredictionsPipeline:
         model_registry = hopsworks_project.get_model_registry()
         model_registry_reference = model_registry.get_model(name="", version=model_version)
         model_dir = model_registry_reference.download()
-        model_path = Path(model_dir)
+        model_path = Path(model_dir) + "lasso.pkl"
         model = load_model(model_path)
         return model 
 
@@ -61,4 +83,10 @@ class BatchPredictionsPipeline:
         Save batch predictions on cloud 
         
         """
+
+    def InitializeBatchPredictionsPipeline(self): 
+        data = self.get_predictions_data(
+            name="batch_predictions_data", 
+            description="batch predictions data for batch predicitions"
+        )
     
